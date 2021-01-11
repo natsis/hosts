29a53f1265e181d959f31ab60d4c2b19a53c0a27
natsis <11601717+natsis@users.noreply.github.com>
2021-01-11 16:37:00 +0200

    Ready?

--- README.md
+++ README.md
@@ -5,4 +5,11 @@ An automated script I use to create hosts list for my linux machines.
 - Includes some lists in the configuration.
 - Merges lists from different sources.
 - Accepts custom url entries.
+- **Will try to update the host file.**
+
+I uses `sudo` to update the `/etc/hosts` file without any warning.
+
+I was inspired from [StevenBlack/hosts](https://github.com/StevenBlack/hosts) and revised a very very old script I had, for the similar purpose.
+Here is my new version.
+
 
--- hosts_list_apply.sh
+++ hosts_list_apply.sh
@@ -29,41 +29,41 @@ validate_url() {
 
 
 
-echo "Runs fron $DIR"
+echo "Runs from $DIR"
 
 
 
 
 ## Get data
 
-# echo "Get text list files"
-# cat "$txtlst_fl" | sed 's/#.*//g' | sed '/^[[:space:]]*$/d' | while read line;do
-#     ii=$((ii+1))
-#     # echo "list $ii: $line
-#     file="$TMP/File_t_${ii}.list"
-# 
-#     if [[ $(validate_url "$line") == "true" ]] ; then
-#         echo "Get: $line"
-#         wget -nc -O "$file"  "$line"
-#     else
-#         echo "NOT FOUND: $line"
-#     fi
-# done    
-# 
-# 
-# echo "Get zip list files"
-# cat "$ziplst_fl" | sed 's/#.*//g' | sed '/^[[:space:]]*$/d' | while read line;do
-#     ii=$((ii+1))
-#     # echo "list $ii: $line
-#     file="$TMP/File_z_${ii}.list"
-# 
-#     if [[ $(validate_url "$line") == "true" ]] ; then
-#         echo "Get: $line"
-#         wget -O- "$line" | zcat > "$file"
-#     else
-#         echo "NOT FOUND: $line"
-#     fi
-# done    
+echo "Get text list files"
+cat "$txtlst_fl" | sed 's/#.*//g' | sed '/^[[:space:]]*$/d' | while read line;do
+    ii=$((ii+1))
+    # echo "list $ii: $line
+    file="$TMP/File_t_${ii}.list"
+
+    if [[ $(validate_url "$line") == "true" ]] ; then
+        echo "Get: $line"
+        wget -nc -O "$file"  "$line"
+    else
+        echo "NOT FOUND: $line"
+    fi
+done    
+
+
+echo "Get zip list files"
+cat "$ziplst_fl" | sed 's/#.*//g' | sed '/^[[:space:]]*$/d' | while read line;do
+    ii=$((ii+1))
+    # echo "list $ii: $line
+    file="$TMP/File_z_${ii}.list"
+
+    if [[ $(validate_url "$line") == "true" ]] ; then
+        echo "Get: $line"
+        wget -O- "$line" | zcat > "$file"
+    else
+        echo "NOT FOUND: $line"
+    fi
+done    
 
 
 
@@ -85,16 +85,87 @@ find "$TMP" -type f -iname "File*.list" | while read line; do
     sed -i 's/^local$//g'                                    "$line"
     sed -i 's/^broadcasthost$//g'                            "$line"
     sed -i 's/^.localdomain$//g'                             "$line"
-    
-#     sed -i '/[ ]*::[0-9]/d'                    "$line"
-#     sed -i '/[ ]*!/d'                          "$line"
-
+    sed -i '/[ ]*::[0-9]/d'                    "$line"
+    sed -i '/[ ]*!/d'                          "$line"
 done
 
 
 
+## Create unified list
+
+cat "${TMP}/"File*.list       > "$maintemp"
+sed 's/#.*//g' "$custom_fl"  >> "$maintemp"
+sort -u -o "$maintemp"        "$maintemp"
+sed -i '/^$/d'                "$maintemp"
+sed -i '/^[ \t]*$/d'          "$maintemp"
+sed -i '/^[[:space:]]*$/d'    "$maintemp"
+sed -i 's/^/0.0.0.0 /'        "$maintemp"
 
 
-echo $ii
+echo 
+echo "Excluded urls"
+wc -l $maintemp
+echo ""
+
+
+
+## Create hosts list file
+
+date +"# Created: %F %R " > "$mainfile"
+echo ""                   >> "$mainfile"
+echo "
+127.0.0.1 localhost
+127.0.0.1 localhost.localdomain
+127.0.0.1 local
+255.255.255.255 broadcasthost
+::1 localhost
+::1 ip6-localhost
+::1 ip6-loopback
+fe80::1%lo0 localhost
+ff00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+0.0.0.0 0.0.0.0
+127.0.1.1 $(hostname)
+127.0.0.53 $(hostname)
+
+"                          >> "$mainfile"
+
+## The final file to use 
+cat "$maintemp" >> "$mainfile"
+
+
+size="$(stat -c%s $mainfile)"
+
+echo ""
+echo "New list size:  $size bytes "
+echo "New list hosts: $(wc -l $maintemp) "
+echo ""
+
+
+## I know you trust me
+sudo mv -v "$mainfile"  '/etc/hosts'
+
+# echo ""
+# read -p "Copy the new list to /etc/hosts y/n? " -r
+# echo ""
+# if [[ $REPLY =~ ^[Yy]$ ]]; then
+#     sudo mv -v "$mainfile"  '/etc/hosts'
+# fi
+
+
+
+## You may want to keep them while coding/debugging
+rm -r "$TMP"
+
+# echo ""
+# read -p "Remove temporary files y/n? " -r
+# echo ""
+# if [[ $REPLY =~ ^[Yy]$ ]]; then
+#     rm -r "$TMP"
+# fi
+
 
 exit 0
