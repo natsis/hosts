36a74595334f04e4504a87b5f92d93396d9e5d65
natsis <11601717+natsis@users.noreply.github.com>
2021-01-11 13:17:05 +0200

    Auto commit 2021-01-11_13:17:05

--- hosts_list_apply.sh
+++ hosts_list_apply.sh
@@ -1,11 +1,95 @@
 #!/bin/bash
 ## created on 2021-01-11
 
+#
+#  Will get files from the internet but it will not overwrite them.
+#  It is polite for the servers. Have to clean temp dir manually.
+#
+
+
+
+## Variables
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+custom_fl="$DIR/manual_host.list"
+txtlst_fl="$DIR/repo_text.list"
+ziplst_fl="$DIR/repo_zip.list"
+
+
+## temp output
+TMP="/dev/shm/hostdir"
+mkdir -p "$TMP"
+
+mainfile="$TMP/mainfile"
+maintemp="$TMP/maintemp"
+
+
+## help function
+validate_url() {
+    if [[ $(wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK') ]]; then echo "true"; fi
+}
+
+
+echo "Runs fron $DIR"
+
+
+
+## Get data
+
+echo "Get text list files"
+cat "$txtlst_fl" | sed 's/#.*//g' | sed '/^[[:space:]]*$/d' | while read line;do
+    ii=$((ii+1))
+    # echo "list $ii: $line
+    file="$TMP/File_t_${ii}.list"
+
+    if [[ $(validate_url "$line") == "true" ]] ; then
+        echo "Get: $line"
+        wget -nc -O "$file"  "$line"
+    else
+        echo "NOT FOUND: $line"
+    fi
+done    
+
+
+echo "Get zip list files"
+cat "$ziplst_fl" | sed 's/#.*//g' | sed '/^[[:space:]]*$/d' | while read line;do
+    ii=$((ii+1))
+    # echo "list $ii: $line
+    file="$TMP/File_z_${ii}.list"
+
+    if [[ $(validate_url "$line") == "true" ]] ; then
+        echo "Get: $line"
+        wget -O- "$line" | zcat > "$file"
+    else
+        echo "NOT FOUND: $line"
+    fi
+done    
+
+
+
+
+## Clean data
+
+find "$TMP" -type f -iname "File*.list" | while read line; do
+    echo "Clean: $line"
+    # rules to homogenize hosts list
+    # you can inspect the results in the temp folder
+    sed -i 's/#.*$//g'                         "$line"
+#     sed -i 's/[ ]*localhost.*//g'              "$line"
+#     sed -i 's/[ ]*local$//g'                   "$line"
+#     sed -i 's/[ \t]*127\.0\.0\.1[ \t]*//g'     "$line"
+#     sed -i 's/[ \t]*0\.0\.0\.0[ \t]*//g'       "$line"
+#     sed -i '/[ \t]*255\.255\.255\.255[ \t]*/d' "$line"
+#     sed -i '/[ ]*::[0-9]/d'                    "$line"
+#     sed -i '/[ ]*!/d'                          "$line"
+
+done
 
 
 
 
 
+echo $ii
 
-
-exit 0 
+exit 0
